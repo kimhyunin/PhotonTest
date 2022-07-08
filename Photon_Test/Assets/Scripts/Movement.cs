d1e4a51119c42e0c39a24e0c89bdb97c68d4ac8c
HyunIn_Asus <kimhyuninnnn@gmail.com>
2022-07-08 23:29:46 +0900

    import Package

---
+++ Photon_Test/Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController controller;
+    private new Transform transform;
+    private Animator animator;
+    private new Camera camera;
+
+    // 가상의 Plane에 레이캐스팅하기 위한 변수
+    private Plane plane; // 가상의 바닥을 만들기 위한 변수
+    private Ray ray;
+    private Vector3 hitPoint;
+
+    //이동속도
+    public float moveSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
